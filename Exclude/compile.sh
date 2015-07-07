e9595b34aab1669e2d8285e7ee4af08ed53c7e1a
jonathanknorn <j.knorn@gmail.com>
2015-07-07 14:36:09 +0200

    removing Exclude?

--- Exclude/compile.sh
+++
@@ -1,5 +0,0 @@
-#g++-4.7 hello.cpp -o  hello -I /usr/local/include/opencv -L/opt/vc/lib -lmmal -lmmal_core -lmmal_util -lopencv_core -lopencv_highgui
-
-g++-4.7 BackgroundSubtraction.cpp -o  BackgroundSubtraction -L/usr/local/lib -lopencv_nonfree -lopencv_objdetect -lopencv_features2d -lopencv_imgproc -lopencv_highgui -lopencv_core   -lopencv_video
-
-#g++-4.7 hello2.cpp -o  hello2 -L/usr/local/lib -lopencv_nonfree -lopencv_objdetect -lopencv_features2d -lopencv_imgproc -lopencv_highgui -lopencv_core   -lopencv_video
